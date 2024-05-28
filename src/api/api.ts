829051d11b98dcff94c31487a2bd1f475ade8fdc
IrynaPoritskaa <130314350+IrynaPoritskaa@users.noreply.github.com>
2024-05-28 13:23:07 +0300

    Update api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import toast from 'react-hot-toast';
 import {errorToasterStyles} from "../constants/toaster";
-const url ='http://localhost:8080/'
+const url ='https://backend-vjn4.onrender.com/'
 export const getTrains=async () => {
     try {
         const response = await axios.get(url + 'trains');
